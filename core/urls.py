358ecfb588c25cece0c8f4c4138fd411ab144ab7
manearalucaioana03 <manearaluca27@gmail.com>
2026-03-15 13:21:57 +0200

    created views.py, form.py and empty login page

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('transactions/', views.TransactionListView.as_view(), name='transaction-list'),
+    path('transactions/add/', views.TransactionCreateView.as_view(), name='transaction-add'),
+    path('transactions/<int:pk>/', views.TransactionDetailView.as_view(), name='transaction-detail'),
+    path('transactions/<int:pk>/edit/', views.TransactionUpdateView.as_view(), name='transaction-edit'),
+    path('transactions/<int:pk>/delete/', views.TransactionDeleteView.as_view(), name='transaction-delete'),
+]
